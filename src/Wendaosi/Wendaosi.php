7e22b4b40e07c0daa76a370206ea6766c74be359
cqkd6381 <cqkd6381@163.com>
2017-01-17 16:56:15 +0800

    add src/Wendaosi/Wendaosi

---
+++ src/Wendaosi/Wendaosi.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the Wendaosi package which is just for test.
+ *
+ * (c) cqkd6381 <cqkd6381@163.com>
+ *
+ */
+namespace Wendaosi;
+
+class Wendaosi
+{
+	protected $msg;
+
+	public function __construct($msg = 'composer test')
+	{
+		$this->msg = $msg;
+	}
+
+	public function printMsg()
+	{
+		return $this->msg;
+	}
+}
